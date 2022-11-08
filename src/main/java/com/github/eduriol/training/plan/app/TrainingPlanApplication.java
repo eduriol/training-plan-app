067f4d97ee0dcff7258d223ff1ff593e3a584e73
Eduardo <eduriol@gmail.com>
2022-11-08 12:19:19 +0100

    First commit with DB dev configuration

---
+++ src/main/java/com/github/eduriol/training/plan/app/TrainingPlanApplication.java
@@ -0,0 +1,13 @@
+package com.github.eduriol.training.plan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/eduriol/training/plan/app/TrainingPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eduriol.training.plan.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
