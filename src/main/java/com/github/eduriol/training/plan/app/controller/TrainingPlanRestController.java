d548dbcd7d2d0112270896c19f4ee7398d33ff60
Eduardo <eduriol@gmail.com>
2023-02-10 22:55:57 +0100

    Add logging to RestController

--- src/main/java/com/github/eduriol/training/plan/app/controller/TrainingPlanRestController.java
+++ src/main/java/com/github/eduriol/training/plan/app/controller/TrainingPlanRestController.java
@@ -5,6 +5,8 @@ import com.github.eduriol.training.plan.app.exception.BadRequestException;
 import com.github.eduriol.training.plan.app.models.HealthStatus;
 import com.github.eduriol.training.plan.app.models.domain.Topic;
 import com.github.eduriol.training.plan.app.service.ITopicService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -19,51 +21,75 @@ import java.util.List;
 @RequestMapping("/api")
 public class TrainingPlanRestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TrainingPlanRestController.class);
+
     @Autowired
     private ITopicService topicService;
 
     @GetMapping("/health")
     public HealthStatus health() {
-        return new HealthStatus();
+        logger.info("Received health check request");
+        HealthStatus healthStatus = new HealthStatus();
+        logger.info("Health check completed, all components are healthy");
+        return healthStatus;
     }
 
     @PostMapping("/topics")
     @ResponseStatus(HttpStatus.CREATED)
     public Topic createTopic(@Valid @RequestBody Topic topic, BindingResult result) {
 
+        logger.info("Received request to create topic: {}", topic);
+
         if (result.hasErrors()) {
             List<String> errors = result.getFieldErrors()
                     .stream()
                     .map(err -> "'" + err.getField() + "' " + err.getDefaultMessage())
                     .toList();
+            logger.warn("Bad request exception: {}", errors);
             throw new BadRequestException(errors);
         }
 
-        return topicService.save(topic);
+        Topic createdTopic = topicService.save(topic);
+        logger.info("Response: {}", createdTopic);
+
+        return createdTopic;
     }
 
     @GetMapping("/topics")
     public List<Topic> getAllTopics() {
-        return topicService.findAll();
+        logger.info("Received request to list all topics");
+        List<Topic> topics = topicService.findAll();
+        logger.info("Response: {}", topics);
+        return topics;
     }
 
     @GetMapping("/topics/{id}")
     public Topic getTopic(@PathVariable Long id) {
+
+        logger.info("Received request to get topic with id = {}", id);
+
         Topic topic = topicService.findById(id);
 
         if (topic == null) {
+            logger.warn("Not found exception for topic with id = {}", id);
             throw new NotFoundException(List.of("The topic with id = " + id.toString() + " does not exist."));
         }
+
+        logger.info("Response: {}", topic);
 
         return topic;
 
     }
 
     @PutMapping("/topics/{id}")
-    public Topic updateTopic(@PathVariable Long id, @Valid @RequestBody Topic updatedTopic, BindingResult result) {
+    public Topic updateTopic(@PathVariable Long id, @Valid @RequestBody Topic newTopic, BindingResult result) {
+
+        logger.info("Received request to update topic with id = {} with topic: {}", id, newTopic);
+
         Topic topic = topicService.findById(id);
 
         if (topic == null) {
+            logger.warn("Not found exception for topic with id = {}", id);
             throw new NotFoundException(List.of("The topic with id = " + id.toString() + " does not exist."));
         }
 
@@ -72,22 +98,33 @@ public class TrainingPlanRestController {
                     .stream()
                     .map(err -> "'" + err.getField() + "' " + err.getDefaultMessage())
                     .toList();
+            logger.warn("Bad request exception: {}", errors);
             throw new BadRequestException(errors);
         }
 
-        topic.setName(updatedTopic.getName());
-        return topicService.save(topic);
+        topic.setName(newTopic.getName());
+        Topic updatedTopic = topicService.save(topic);
+        logger.info("Response: {}", updatedTopic);
+
+        return updatedTopic;
 
     }
 
     @DeleteMapping("/topics/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTopic(@PathVariable Long id) {
+
+        logger.info("Received request to delete topic with id = {}", id);
+
         try {
             topicService.delete(id);
         } catch (EmptyResultDataAccessException ex) {
+            logger.warn("Not found exception for topic with id = {}", id);
             throw new NotFoundException(List.of("The topic with id = " + id.toString() + " does not exist."));
         }
+
+        logger.info("Topic with id = {} successfully deleted", id);
+
     }
 
 }
