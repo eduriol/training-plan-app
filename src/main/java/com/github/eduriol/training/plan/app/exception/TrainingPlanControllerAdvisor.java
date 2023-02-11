d53d78ae52e32fbc6c6628c9d1f5b7bac8188dda
Eduardo <eduriol@gmail.com>
2023-02-11 16:34:01 +0100

    Adding logs to service implementation

--- src/main/java/com/github/eduriol/training/plan/app/exception/TrainingPlanControllerAdvisor.java
+++ src/main/java/com/github/eduriol/training/plan/app/exception/TrainingPlanControllerAdvisor.java
@@ -1,5 +1,7 @@
 package com.github.eduriol.training.plan.app.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +17,18 @@ import java.util.Map;
 @ControllerAdvice
 public class TrainingPlanControllerAdvisor extends ResponseEntityExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(TrainingPlanControllerAdvisor.class);
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Object> handleApplicationException(ApplicationException ex, WebRequest request) {
         Map<String, List<String>> bodyOfResponse = new HashMap<>();
         bodyOfResponse.put("errors", ex.getErrors());
         HttpStatus responseStatus;
         if (ex instanceof NotFoundException) {
-            logger.warn("Not found exception", ex);
+            logger.warn("Not found exception: {}", ex.getMessage());
             responseStatus = HttpStatus.NOT_FOUND;
         } else if (ex instanceof BadRequestException) {
-            logger.warn("Bad request exception", ex);
+            logger.warn("Bad request exception: {}", ex.getMessage());
             responseStatus = HttpStatus.BAD_REQUEST;
         } else {
             logger.error("Illegal argument exception", ex);
--- src/main/java/com/github/eduriol/training/plan/app/service/TopicServiceImpl.java
+++ src/main/java/com/github/eduriol/training/plan/app/service/TopicServiceImpl.java
@@ -2,6 +2,8 @@ package com.github.eduriol.training.plan.app.service;
 
 import com.github.eduriol.training.plan.app.dao.ITopicDao;
 import com.github.eduriol.training.plan.app.models.domain.Topic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,30 +13,43 @@ import java.util.List;
 @Service
 public class TopicServiceImpl implements ITopicService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TopicServiceImpl.class);
+
     @Autowired
     private ITopicDao topicDao;
 
     @Override
     @Transactional
     public Topic save(Topic topic) {
-        return topicDao.save(topic);
+        logger.debug("Saving topic data {}", topic);
+        Topic savedTopic = topicDao.save(topic);
+        logger.debug("Saved topic data {}", savedTopic);
+        return savedTopic;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<Topic> findAll() {
-        return (List<Topic>) topicDao.findAll();
+        logger.debug("Getting list of topics");
+        List<Topic> topics = (List<Topic>) topicDao.findAll();
+        logger.debug("Found {} topics", topics.size());
+        return topics;
     }
 
     @Override
     @Transactional(readOnly = true)
     public Topic findById(Long id) {
-        return topicDao.findById(id).orElse(null);
+        logger.debug("Looking for topic with id = {}", id);
+        Topic topic = topicDao.findById(id).orElse(null);
+        logger.debug("Found topic {}", topic);
+        return topic;
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
+        logger.debug("Deleting topic with id = {}", id);
         topicDao.deleteById(id);
+        logger.debug("Topic deleted");
     }
 }
